a31aff14cb023bd3ea75a1fc91a4e98d7cc5fb93
Abdelhak Brahem <abdelhakbrahem@gmail.com>
2021-11-07 23:10:09 +0100

    message

--- myScript.js
+++ myScript.js
@@ -1,25 +1,33 @@
-// $('document').ready(function(){
-//     var count = 0;
-//     $('.step').each(function(){
-//       count++;
-     
-//     });
-     
-    var i = 0;
-    //NEXT
-    document.getElementById('#next').on('click', function(){
-      if(count > i +1 ){
-          i++;
-          document.getElementsByClassName('.active').removeClass('active');
-          document.getElementsByClassName('.step').eq(i).addClass('active');
-         }
-    });
-      //PREV
-    // $('#prev').on('click', function(){
-    //  if(i > 0 ){
-    //   i--;
-    //   $('.active').removeClass('active');
-    //   $('.step').eq(i).addClass('active');
-    //  }
-    // });
-//   });
+let step = 'step1';
+
+const step1 = document.getElementById('stepText1');
+const step2 = document.getElementById('stepText2');
+const step3 = document.getElementById('step3');
+const step4 = document.getElementById('step4');
+
+function next() {
+  if (step === 'stepText1') {
+    step = 'stepTxt2';
+    step1.classList.add("ok");
+    step1.classList.remove("active");
+    step2.classList.add('active');
+
+  } else if (step === 'step2') {
+    step = 'step3';
+    step2.classList.add("ok");
+    step2.classList.remove("active");
+    step3.classList.add('active');
+
+  } else if (step === 'step3') {
+    step = 'step4';
+    step3.classList.add("ok");
+    step3.classList.remove("active");
+    step4.classList.add('active');
+
+  } else if (step === 'step4') {
+    step = 'complete';
+    step4.classList.add("ok");
+    step4.classList.remove("active");
+
+    }
+  }
